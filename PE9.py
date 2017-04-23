469a3f8cae173b3c2a1f93e390037eb9be88e5b5
tirthjain <jaintirth24@gmail.com>
2017-04-23 16:09:32 +0530

    Added comment.

--- PE9.py
+++ PE9.py
@@ -1,6 +1,7 @@
 import time
 from math import sqrt
 # Brute Force
+#Takes ~22 seconds
 def pythagorean_triplet1(perimeter):
     for i in range(1, perimeter):
         for j in range(1, perimeter):
@@ -10,6 +11,7 @@ def pythagorean_triplet1(perimeter):
 
 
 # Euclid's Formula
+#Takes less than 0.1 second.
 def pythagorean_triplet2(perimeter):
     for m in range(1, perimeter - 1):
         for n in range(1, perimeter - m):
